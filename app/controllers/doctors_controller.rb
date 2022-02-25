58a3ce039dc7c9786f0abf0aac43b18e29ebf0d4
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-02-25 11:00:13 +0100

    Add rubocop and fix errors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,5 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: %i[ show update destroy ]
+  before_action :set_doctor, only: %i[show update destroy]
 
   # GET /doctors
   def index
@@ -39,13 +39,14 @@ class DoctorsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_doctor
-      @doctor = Doctor.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def doctor_params
-      params.require(:doctor).permit(:name, :location, :specification, :picture, :price, :description, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def doctor_params
+    params.require(:doctor).permit(:name, :location, :specification, :picture, :price, :description, :user_id)
+  end
 end
