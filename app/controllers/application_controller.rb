aaf2ef04125da80eca0a4663ebd3bd973b80681a
Ahmet Bozaci <ahmt9417@gmail.com>
2022-02-28 14:08:36 +0300

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,27 +3,29 @@ class ApplicationController < ActionController::API
     JWT.encode(payload, 'my_secret')
   end
 
-  def auth_header
-    request.headers['Authorization']
-  end
+  # ! USE IT AFTER CREATE REACT
 
-  def decoded_token
-    if auth_header
-      token = auth_header.split[1]
-      render json: { failure: auth_header }
-      begin
-        JWT.decode(token, 'my_secret', true, algorithm: 'HS256')
-      rescue JWT::DecodeError
-        []
-      end
-    end
-  end
+  # def auth_header
+  #   request.headers['Authorization']
+  # end
 
-  def current_user
-    decoded_hash = decoden_token
-    unless decoded_has.empty?
-      user_id = decoded_hash[0]['user_id']
-      @usuer = User.find_by(id: user_id)
-    end
-  end
+  # def decoded_token
+  #   return auth_header
+  #     token = auth_header.split[1]
+  #     render json: { failure: auth_header }
+  #     begin
+  #       JWT.decode(token, 'my_secret', true, algorithm: 'HS256')
+  #     rescue JWT::DecodeError
+  #       []
+  #     end
+  #   end
+  # end
+
+  # def current_user
+  #   decoded_hash = decoden_token
+  #   unless decoded_has.empty?
+  #     user_id = decoded_hash[0]['user_id']
+  #     @usuer = User.find_by(id: user_id)
+  #   end
+  # end
 end
