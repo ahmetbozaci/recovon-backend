2c55435bbe267d3625d2ed88980174c83d14adda
ali abbani <aliox@ahlam.com>
2022-02-28 15:17:52 +0200

    Add cancancan gem and generate an ability file to manage roles

--- db/migrate/20220225084612_create_users.rb
+++ db/migrate/20220225084612_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
+      t.boolean :admin
       t.string :password_digest
       t.string :email
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_101311) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.boolean "admin"
     t.string "password_digest"
     t.string "email"
     t.datetime "created_at", null: false
