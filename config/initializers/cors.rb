dc4913d9df4fc8bbebe578f6230c4a199238be50
ali abbani <aliox@ahlam.com>
2022-03-02 11:38:26 +0200

    update the core file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://final-api-44.herokuapp.com/"
+    origins "*"
 
     resource "*",
       headers: :any,
