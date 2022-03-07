f9fe679915b1c836cd1318ec2271d631e094e4ee
ali abbani <aliox@ahlam.com>
2022-03-07 14:13:08 +0200

    Update the current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
-  # def current_user
-  #   User.first
-  # end
+  def current_user
+    User.first
+  end
 
   def encode_token(payload)
     JWT.encode(payload, 'my_secret')
