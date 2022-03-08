2a94b96cd6b1e9f02b3b3d0431f18894d574b810
ali abbani <aliox@ahlam.com>
2022-03-08 15:53:45 +0200

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ end
 @doctor3 = Doctor.create!(user: @user1, name: 'Ali Abbani', location: 'Lebanon', specification: 'Plastic surgery', picture: 'https://cdn.pixabay.com/photo/2021/02/09/06/44/doctor-5997500__340.jpg', price: '95', description: 'willing to go the extra mile for his patients')
 @doctor4 = Doctor.create!(user: @user1, name: 'Henry Eze', location: 'Nigeria', specification: 'Dentist', picture: 'https://media.istockphoto.com/photos/if-you-need-me-just-call-me-picture-id1288813313?k=20&m=1288813313&s=612x612&w=0&h=NiAjImsLoy5_ssvIjtMdXmG5Hqmm8uYdIpNhfqfuvHE=', price: '80', description: 'do whatever it takes to help meet his patients’ needs')
 @doctor5 = Doctor.create!(user: @user1, name: 'William Pedro', location: 'Brazil', specification: 'geneticist ', picture: 'https://image.shutterstock.com/image-photo/cheerful-mature-doctor-posing-smiling-260nw-1384243295.jpg', price: '110', description: 'friendly and caring')
-@doctor10 = Doctor.create!(user: @user10, name: 'Benis Naah', location: 'France', specification: 'Dermatologists', picture: 'https://images.pexels.com/photos/5452201/pexels-photo-5452201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', price: '115', description: 'vigilant about following up on any tests that are done')
+@doctor10 = Doctor.create!(user: @user1, name: 'Benis Naah', location: 'France', specification: 'Dermatologists', picture: 'https://images.pexels.com/photos/5452201/pexels-photo-5452201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', price: '115', description: 'vigilant about following up on any tests that are done')
 
 @appointment1 = Appointment.create!(user:@user1, doctor: @doctor1, time: Time.now, date: '24-02-2022')
 @appointment2 = Appointment.create!(user:@user1, doctor: @doctor2, time: Time.now, date: '25-02-2022')
@@ -38,7 +38,7 @@ end
 @appointment4 = Appointment.create!(user:@user1, doctor: @doctor4, time: Time.now, date: '27-02-2022')
 @appointment5 = Appointment.create!(user:@user1, doctor: @doctor5, time: Time.now, date: '28-02-2022')
 @appointment6 = Appointment.create!(user:@user1, doctor: @doctor1, time: Time.now, date: '1-03-2022')
-@appointment10 = Appointment.create!(user:@user10, doctor: @doctor10, time: Time.now, date: '1-03-2022')
+@appointment10 = Appointment.create!(user:@user1, doctor: @doctor10, time: Time.now, date: '1-03-2022')
 
 # @user2 = User.create!(name: 'user2', password: "123456789", password_confirmation: "123456789", email:"user2@test.com")
 # @user3 = User.create!(name: 'user3', password: "123456789", password_confirmation: "123456789", email:"user3@test.com")
