f728b8f6d8b4b01329beeb4127644ec73d7c612d
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-03-08 17:20:16 +0100

    Set name and email validation for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe User, type: :model do
     subject { User.new(name: 'Ali', email: 'ali@mail.com', password: '12345678', password_confirmation: '12345678') }
     before { subject.save }
 
-    it 'name should be present' do
+    it 'name should be present for a user' do
       subject.name = nil
       expect(subject).to_not be_valid
     end
 
-    it 'email should be present' do
+    it 'email of user should be present' do
       subject.email = nil
       expect(subject).to_not be_valid
     end
