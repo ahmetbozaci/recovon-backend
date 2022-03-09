14197c41fdb8c9386b8b2e4f4ae7ef5f2eff6cd3
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-09 12:37:40 +0300

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
 
-  # GET /users
-  def index
-    @users = User.all
-    render json: { users: @users }
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
   def create
     @user = User.create(user_params)
     if @user.valid?
@@ -25,28 +13,12 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.permit(:name, :password, :password_confirmation, :email, :admin)
   end
