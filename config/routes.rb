f0d696ccb46dfa06b4c312f98d2b528afbcee8e6
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-11 03:17:09 +0300

    Fix syntax error in route file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :appointments, only: [:index, :show, :create, :destroy]
   resources :doctors, only: [:index, :show, :create]
   root "doctors#index"
-  resources :users only: :index
+  resources :users, only: :index
   post "/signup", to: "users#create"
   post "/login", to: "sessions#login"
   post "/logout", to: "sessions#logout"
