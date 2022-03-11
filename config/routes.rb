1d3dd235fe488cf0b29f48d803d7266449d36cd3
Ahmet Bozaci <ahmt9417@gmail.com>
2022-03-11 13:34:33 +0300

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :appointments, only: [:index, :show, :create, :destroy]
   resources :doctors, only: [:index, :show, :create]
-  root "doctors#index"
   resources :users, only: :index
   post "/signup", to: "users#create"
   post "/login", to: "sessions#login"
